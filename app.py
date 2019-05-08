199e0020a4c6d4f5a870e1c4ce6c58890afc7792
harjot saroya <harjot.saroya@mail.utoronto.ca>
2019-05-08 16:42:51 -0400

    added comments

--- app.py
+++ app.py
@@ -17,13 +17,11 @@ def get_db():
     return db
 
 # converts the tuples from get_db() into dictionaries
-# (don't worry if you don't understand this code)
 def make_dicts(cursor, row):
     return dict((cursor.description[idx][0], value)
                 for idx, value in enumerate(row))
 
 # given a SELECT query, executes and returns the result
-# (don't worry if you don't understand this code)
 def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
     rv = cur.fetchall()
@@ -51,89 +49,127 @@ def index():
 def homepage():
     return render_template('homepage.html')
 
+# Login
 @app.route('/login', methods = ['GET','POST'])
 def login():
+    # Get the database
     db = get_db()
+    # Make database entries into dictionaries
     db.row_factory = make_dicts
+    # Get login information from user
     info = request.form
+    # Query the database for the user based on username and password
     query = query_db('select * from user where username = ? and password = ?', [info['username'],info['password']], one = True)
+    # Declare a variable to detect when we've found the user
     found = False
+    # Close database
     db.close()
+    # If our query is not none ie. we've found our user, set found to true
     if (query):
         found = True
     else:
+        # Otherwise flash that the credentials are not valid
         flash('Incorrect credentials')
         flash('Please try again')
+        # Return to index
         return redirect(url_for('index'))
+    # If we have found our user
     if (found == True):
+        # Check if the username exists
         if (query['username'] != None):
+            # Check if the password is correct
             if (query['password'] == info['password']):
+                # Set the session username to our current user's username
                 session['username'] = info['username']
+                # Set the session type to our current user's user type
                 session['type'] = query['user_type']
+                # Enter the homepage and pass user information to homepage
                 return render_template('homepage.html',query = query)
+            # If the password is wrong flash incorrect credentials and return
+            # To the login page
             else:
                 flash('Incorrect credentials')
                 return redirect(url_for('login'))
     return redirect(url_for('login'))
 
+# New user creation
 @app.route('/create-user', methods = ['GET','POST'])
 def newUser():
     db = get_db()
     db.row_factory = make_dicts
+    # Define a cursor to traverse our queries
     cur = db.cursor()
+    # Get new user information
     info = request.form
+    # Insert new user information into our user database
     cur.execute('insert into user (username,password,user_type) values (?,?,?)', [info['username'],info['password'],info['user_type']])
+    # Commit changes
     db.commit()
     cur.close()
+    # Flash that a new user was created and prompt login
     flash('User was sucessfully created!')
     flash('Please login')
     return redirect(url_for('index'))
 
 
 @app.route('/logout')
-#@login_required
 def logout():
+    # Pop user's username
     session.pop('username', None)
+    # Pop user's user type
     session.pop('type', None)
+    # If we have created a session for student (as an instructor) then pop it
+    if (session['student']):
+        session.pop('student',None)
     return redirect(url_for('index'))
 
 @app.route('/marks')
 def marks():
+    # If our current user is an instructor allow mark editing
     if(session['type'] == 'instructor'):
         db = get_db()
         db.row_factory = make_dicts
+        # Query all students in the database to show instructor which students are
+        # Presently in his class
         query = query_db('select id,username from user where user_type = ?',['student'])
         db.close()
         return render_template('studentlist.html',items = query)
+    # If our current user is a student then get their username from session
     user = session['username']
     db = get_db()
     db.row_factory = make_dicts
     items = []
+    # Retrieve marks from the database for our current user
     query = query_db('select * from marks where username = ?',[user], one = True)
     items.append(query)
     db.close()
     return render_template('marks.html',query = items)
 
+# This is for the instructor to view marks for a specific student
 @app.route('/viewmark', methods = ['GET','POST'])
 def viewmark():
     db = get_db()
     db.row_factory = make_dicts
+    # Get information from instructor on desired student
     student = request.form
+    # Create a new session variable called student which has the name of the student
     session['student'] = student['user']
+    # Query the marks for this student
     query = query_db('select * from marks where username = ?',[student['user']])
     return render_template('editmarks.html',items = query,name = student['user'])
 
+# This allows instructors to edit marks for a student
 @app.route('/editmark', methods = ['GET','POST'])
 def editmark():
     db = get_db()
     db.row_factory = make_dicts
+    # Get information like grade and evaluation from instructor
     form = request.form
-    print('1')
-    print(form)
     cur = db.cursor()
     evaluation = form['evaluation']
     grade = form['grade']
 
+    # Create cases for each evaluation and update them accordingly
     if (evaluation == 'quiz1'):
         cur.execute('update marks set quiz1 = ? where username = ?',(grade,session['student']))
         db.commit()
@@ -156,7 +192,7 @@ def editmark():
     cur.close()
     return redirect(url_for('marks'))
 
-
+# Allows student to send in a remark request
 @app.route('/remark',methods = ['GET','POST'])
 def remark():
     # Get information from the student via form
@@ -174,6 +210,7 @@ def remark():
     flash('Remark request submitted!')
     return redirect(url_for('marks'))
 
+# Allows students to create feedback and instructors to view feedback
 @app.route('/feedback', methods = ['GET','POST'])
 def feedback():
     # Check if current user is a student
